e49183de7dd4ae252a306579a6a14a84f022b78d
dwells2024 <davidwells2024@u.northwestern.edu>
2022-12-01 01:05:49 -0600

    added more to projects 12/1

--- main.js
+++ main.js
@@ -12,7 +12,22 @@ var projects = [
         img: "imgs/caucus.png",
         alt: "Students discuss policy ideas at a participatory budgeting caucus at Northwestern University.",
         description: "I was a member of the Civic Engagement Progam's RAP team. We worked on creating a framework for canvassing residents for participatory budgeting. Our plan is currently being implemented as part of the City of Evanston's new participatory budgeting process."
+    },
+    {
+        name: "The Aquasystem",
+        date: "January 2021 - March 2021",
+        img: "imgs/aquasystem.jpg",
+        alt: "Image of a prototype for The Aquasystem. A box frame with rows of re-used water bottles serving as the floatation device and planting area for native plants.",
+        description: "As part of Northwestern's Design Thinking and Communication Course, I worked with a team to create a floating garden for native plants that the Friends of the Chicago River could attach to the seawall. The design is a hydroponic bed with rows of re-used plastic bottles that serve as the floatation device."
+    },
+    {
+        name: "The Happier Tail",
+        date: "January 2021 - March 2021",
+        img: "imgs/happy_tail.png",
+        alt: "Sketch of the final design of The Happier Tail, a sleeve covering the tail secured around the dog's hind legs.",
+        description: "As part of Northwestern's Design Thinking and Communication Course, I worked with a team to develop a solution for Happy Tail Syndrome for a local veterinarian. The product is a tail sleeve designed to reduce the risk of re-injury of a tail due to excessive wagging."
     }
+
 ]
 
 function createProjects(projects, id){
